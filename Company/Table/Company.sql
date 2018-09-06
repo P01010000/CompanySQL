5a7e45c0f08fd9bab3e4c4d67efe549efc056860
Heiner Pöpping <heiner.poepping@t-online.de>
2018-09-06 17:38:00 +0200

    Removed PersonId from Company and Employee

--- Company/Table/Company.sql
+++ Company/Table/Company.sql
@@ -4,6 +4,5 @@
 	[Name] NVARCHAR(256) NOT NULL,
 	[Description] NVARCHAR(1024),
 	[FoundedAt] DATETIME2 NOT NULL,
-	[Branch] NVARCHAR(256),
-	[PersonId] int NOT NULL REFERENCES Person(id)
+	[Branch] NVARCHAR(256)
 )
--- Company/Table/Employee.sql
+++ Company/Table/Employee.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[Employee]
 (
 	[Id] INT NOT NULL PRIMARY KEY identity(1,1),
-	PersonId int not null references person(id),
 	LastName NVARCHAR(128) NOT NULL,
 	FirstName NVARCHAR(128) NOT NULL,
 	Birthday Date NOT NULL,
